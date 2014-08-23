869a93f91ea8a3aacaab93d164b50edb305f2e66
Andrew Varvel <andy@fastmail.com.au>
2014-08-23 17:40:06 +1000

    Initial Commit

---
+++ Contacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Contacts
+//
+//  Created by ANDREW VARVEL on 23/08/2014.
+//  Copyright (c) 2014 ANDREW VARVEL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
