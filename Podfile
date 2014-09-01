fa07f03333d438377cc8a3e3a421004259ef1548
Andrew Varvel <andy@fastmail.com.au>
2014-09-01 12:12:12 +1000

    Added Tab Bar Controller

---
+++ Podfile
@@ -0,0 +1,2 @@
+platform :ios, '7.0'
+pod 'HPReorderTableView', '~> 0.2'
---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// HPReorderTableView
+#define COCOAPODS_POD_AVAILABLE_HPReorderTableView
+#define COCOAPODS_VERSION_MAJOR_HPReorderTableView 0
+#define COCOAPODS_VERSION_MINOR_HPReorderTableView 2
+#define COCOAPODS_VERSION_PATCH_HPReorderTableView 0
+
